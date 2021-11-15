47b417e87f8e389ad0ed0ebe2adcbc475627684a
Nandhakishore-0809 <nandha.nkishore97@gmail.com>
2021-11-15 18:28:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wn9saj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wn9saj)
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCommonModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatChipsModule,
+    MatCommonModule,
+    MatInputModule,
+    MatGridListModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
